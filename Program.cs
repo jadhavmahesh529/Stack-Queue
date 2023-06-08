93f3e227bdd794d2bf1083ca4f83eea36d7d713e
“Enter <jadhavmahesh529@gmail.com>
2023-06-08 18:07:34 +0530

    Add: Push code is added

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Stack_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Queue
+{
+    internal class Stack<T>
+    {
+        private Node<T> top;
+
+        public Stack()
+        {
+            top = null;
+        }
+
+        public void Push(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+
+            if (top == null)
+            {
+                top = newNode;
+            }
+            else
+            {
+                newNode.Next = top;
+                top = newNode;
+            }
+        }
+
+
+        public void Display()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is empty.");
+                return;
+            }
+
+            Node<T> current = top;
+            while (current != null)
+            {
+                Console.WriteLine(current.Data);
+                current = current.Next;
+            }
+        }
+
+    }
+
+}
