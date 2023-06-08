a42f3a38f5ee909c2a62181041e696b3c0418f61
“Enter <jadhavmahesh529@gmail.com>
2023-06-08 18:20:45 +0530

    Add: Enqueue code is added

--- Program.cs
+++ Program.cs
@@ -4,22 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Stack<int> stack = new Stack<int>();
-            stack.Push(70);
-            stack.Push(30);
-            stack.Push(56);
-            stack.Display();
+            //Stack<int> stack = new Stack<int>();
+            //stack.Push(70);
+            //stack.Push(30);
+            //stack.Push(56);
+            //stack.Display();
 
-            Console.WriteLine("\n");
-            stack.Pop();
-            stack.Display();
-            while (!stack.IsEmpty())
-            {
-                Console.WriteLine("Peek: " + stack.Peek());
-                Console.WriteLine("Pop: " + stack.Pop());
-            }
-            Console.WriteLine("\n");
-            stack.Display();
+            //Console.WriteLine("\n");
+            //stack.Pop();
+            //stack.Display();
+            //while (!stack.IsEmpty())
+            //{
+            //    Console.WriteLine("Peek: " + stack.Peek());
+            //    Console.WriteLine("Pop: " + stack.Pop());
+            //}
+            //Console.WriteLine("\n");
+            //stack.Display();
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Display();
         }
     }
 }
---
+++ Queue.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Queue
+{
+    internal class Queue<T>
+    {
+        public Node<T> head;
+        public Node<T> tail;
+
+        public void Enqueue(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+
+            if (head == null)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                tail.Next = newNode;
+                tail = newNode;
+            }
+        }
+        public void Display()
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                Console.WriteLine(current.Data);
+                current = current.Next;
+            }
+        }
+    }
+}
